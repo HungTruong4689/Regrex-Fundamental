15af5c798bf5c6d77342fc1c333d2fb4081b16a7
HungTruong4689 <hungphuc4689@gmail.com>
2023-03-18 21:06:38 +0200

    fixing something in everything but a new line

--- introduction/matchingEverythingButANewLine.py
+++ introduction/matchingEverythingButANewLine.py
@@ -1,5 +1,19 @@
 regex_pattern = r"^(...\.){3}...$"	# Do not delete 'r'.
 
+
+# Regex Pattern:
+# .
+# ├── ^
+# │   └── Denotes the start of the line
+# ├── (...\.){3}
+# │   ├── ... - Denotes any 3 characters
+# │   ├── \. - Denotes a '.' character
+# │   └── {3} - Denotes the above expression for 3 times
+# ├── ...
+# │   └── Denotes any 3 characters
+# └── $
+#     └── Denotes the end of the line
+
 import re
 import sys
 
